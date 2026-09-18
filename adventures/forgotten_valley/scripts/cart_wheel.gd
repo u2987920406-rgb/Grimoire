0ad263765510379c01e3780b65dbc885c8889c95
u2987920406-rgb <u2987920406@gmail.com>
2026-09-18 19:35:57 +0200

    fix(adventure): qualify object recovery importance

--- adventures/forgotten_valley/scripts/cart_wheel.gd
+++ adventures/forgotten_valley/scripts/cart_wheel.gd
@@ -4,7 +4,7 @@ var attached_to_cart: bool = false
 
 func _ready() -> void:
 	interaction_name = "Roue"
-	importance = Importance.CRITICAL
+	importance = CarryableBody.Importance.CRITICAL
 	recovery_radius = 16.0
 	super._ready()
 
