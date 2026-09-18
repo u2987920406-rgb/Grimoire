22c488d16e430b28905fcc6014b9bfcd8cd7a0d9
u2987920406-rgb <u2987920406@gmail.com>
2026-09-18 19:34:37 +0200

    feat(world): recover important objects lost outside play area

--- core/world_object/carryable_body.gd
+++ core/world_object/carryable_body.gd
@@ -1,15 +1,40 @@
 class_name CarryableBody
 extends RigidBody3D
 
+enum Importance {
+	NON_CRITICAL,
+	RECOVERABLE,
+	CRITICAL,
+}
+
 @export var interaction_name: String = "Objet"
+@export var importance: Importance = Importance.NON_CRITICAL
+@export var recovery_radius: float = 18.0
+@export var recovery_min_y: float = -2.5
+
 var is_held: bool = false
 var _saved_collision_layer: int
 var _saved_collision_mask: int
+var _recovery_transform: Transform3D
 
 func _ready() -> void:
 	add_to_group("interactable")
 	_saved_collision_layer = collision_layer
 	_saved_collision_mask = collision_mask
+	_recovery_transform = global_transform
+
+func _physics_process(_delta: float) -> void:
+	if is_held or importance == Importance.NON_CRITICAL:
+		return
+
+	var origin := _recovery_transform.origin
+	var horizontal_distance := Vector2(
+		global_position.x - origin.x,
+		global_position.z - origin.z
+	).length()
+
+	if global_position.y < recovery_min_y or horizontal_distance > recovery_radius:
+		recover_to_safe_position()
 
 func get_interaction_label(_player: PlayerController) -> String:
 	return "Prendre " + interaction_name.to_lower()
@@ -29,6 +54,15 @@ func set_held(value: bool) -> void:
 	else:
 		collision_layer = _saved_collision_layer
 		collision_mask = _saved_collision_mask
+
+func remember_safe_position() -> void:
+	_recovery_transform = global_transform
+
+func recover_to_safe_position() -> void:
+	linear_velocity = Vector3.ZERO
+	angular_velocity = Vector3.ZERO
+	global_transform = _recovery_transform
+	sleeping = true
 
 func on_picked_up() -> void:
 	set_held(true)
