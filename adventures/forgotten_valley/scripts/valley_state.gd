564b58dba88968e5298663735558cf9f6c89ad38
u2987920406-rgb <u2987920406@gmail.com>
2026-09-21 12:09:36 +0200

    feat(ruins): track ancient network balance

--- adventures/forgotten_valley/scripts/valley_state.gd
+++ adventures/forgotten_valley/scripts/valley_state.gd
@@ -6,12 +6,16 @@ var mill_restored: bool = false
 var field_watered: bool = true
 var sun_disc_revealed: bool = false
 var ruins_open: bool = false
+var ruins_flow_setting: int = 0
+var ancient_network_restored: bool = false
 var adventure_complete: bool = false
 
 signal water_changed(setting: int)
 signal mill_restored_changed(value: bool)
 signal sun_disc_revealed_changed(value: bool)
 signal ruins_opened
+signal ruins_flow_changed(setting: int)
+signal ancient_network_restored_changed(value: bool)
 signal adventure_completed
 
 func set_water_setting(value: int) -> void:
@@ -34,6 +38,14 @@ func open_ruins() -> void:
 		return
 	ruins_open = true
 	ruins_opened.emit()
+
+func set_ruins_flow_setting(value: int) -> void:
+	ruins_flow_setting = clampi(value, 0, 2)
+	var restored := ruins_flow_setting == 1
+	if restored != ancient_network_restored:
+		ancient_network_restored = restored
+		ancient_network_restored_changed.emit(ancient_network_restored)
+	ruins_flow_changed.emit(ruins_flow_setting)
 
 func complete_adventure() -> void:
 	if adventure_complete:
