136ab6532adfe969e745716b30234921066f1996
u2987920406-rgb <u2987920406@gmail.com>
2026-09-22 12:46:38 +0200

    feat(2d): add dynamic illustrated village prototype

---
+++ adventures/forgotten_valley/scripts/2d/hotspot_2d.gd
@@ -0,0 +1,17 @@
+class_name AdventureHotspot2D
+extends Area2D
+
+signal activated(hotspot_id: String)
+
+@export var hotspot_id: String = ""
+@export var label: String = "Examiner"
+@export_multiline var message: String = ""
+
+func _ready() -> void:
+	input_event.connect(_on_input_event)
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		activated.emit(hotspot_id)
+	elif event is InputEventScreenTouch and event.pressed:
+		activated.emit(hotspot_id)
